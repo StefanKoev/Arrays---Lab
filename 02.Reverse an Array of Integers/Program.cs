58d551389325d8ccb530748ecfe8931729eeca9f
Stefan Koev <koev.stefan@gmail.com>
2017-11-03 01:38:59 +0200

    Create 02. Reverse an Array of Integers

---
+++ 02.Reverse an Array of Integers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Reverse_an_Array_of_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var array = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = n - 1; i >= 0; i--)
+            {
+                Console.Write(array[i] + " ");
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
